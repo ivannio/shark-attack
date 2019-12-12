8f84e580ac5fbabf986c52ddfe4e6399ca2664e4
Ivan Phelps <ivanniothemannio@gmail.com>
2019-12-11 19:14:07 -0600

    student data complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,21 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-warning">click me bro</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-warning">click me bro</button>
     </div>
   );
 }
